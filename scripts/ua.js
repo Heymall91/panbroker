0819bbe169fc06f1defad5332e703c9a69d68fb6
Heymall91 <149232993+Heymall91@users.noreply.github.com>
2024-11-19 17:09:41 +0100

    ua.js

--- scripts/ua.js
+++ scripts/ua.js
@@ -194,7 +194,7 @@ function ukrVersion() {
             </ul>
           </div>
           <div class="footer__contact">
-            <a href="tel:+38096 870 6477">+38 096 870 6477</a>
+            <a href="tel:+38096 870 6477">+38 000 000 0000</a>
           </div>
         </div>
         <div class="footer__sign">
